e4b5b17c85d2ccceca7f553a538ae8fb4cebcadc
Alex <dranaxum@dranaxum-Satellite-Pro-C660.(none)>
2012-11-17 21:21:35 +0000

    your mama

--- gen/com/wikitude/example/BuildConfig.java
+++ gen/com/wikitude/example/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.Good;
+package com.wikitude.example;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- src/com/Good/MainActivity.java
+++ src/com/Good/MainActivity.java
@@ -1,14 +1,16 @@
 package com.Good;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+
 import com.Good.Geo.GeoLocation;
 import com.Good.Geo.GeoLocation.NoBearing;
+import com.wikitude.example.R;
 
 public class MainActivity extends Activity {
 
